ea2b5674b081d15e01a9684f099392f566a496a5
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-13 19:52:08 -0500

    Add functionality to allow creation of new posts via frontend form

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,21 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function PostsNew() {
+  const [errors, setErrors] = useState([]);
+
+  const handleNewPost = (event) => {
+    event.preventDefault();
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/posts", params)
+      .then((response) => {
+        event.target.reset();
+        window.location.href = "/";
+      })
+      .catch((errors) => {
+        setErrors(errors.response.data.errors);
+      });
+  };
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleNewPost}>
         <div>
           Title:
-          <input type="text" />
+          <input name="title" type="text" />
         </div>
         <div>
           Body:
-          <input type="text" />
+          <input name="body" type="text" />
         </div>
         <div>
           Image:
-          <input type="url" />
+          <input name="image" type="url" />
         </div>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
