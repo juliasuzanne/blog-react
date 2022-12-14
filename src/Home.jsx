34ab5d8a1d3bb7815132a135e14980fa3cdd50b8
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-13 22:08:03 -0500

    add update functionality through modals

--- src/Home.jsx
+++ src/Home.jsx
@@ -36,6 +36,23 @@ export function Home() {
     axios.post("http://localhost:3000/posts", params).then((response) => setPosts([...posts, response.data]));
   };
 
+  const handleUpdatePost = (id, params) => {
+    axios.patch("http://localhost:3000/posts/" + id + ".json", params).then((response) => {
+      console.log(response.data);
+      setIsPostsShowVisible(false); // hide modal
+      setPosts(
+        posts.map((post) => {
+          if (post.id === id) {
+            //update matching id, otherwise do nothing
+            return response.data;
+          } else {
+            return post;
+          }
+        })
+      );
+    });
+  };
+
   useEffect(handleIndexPosts, []);
   return (
     <div>
@@ -44,7 +61,7 @@ export function Home() {
       <PostsNew onCreatePost={handleCreatePost} />
       <PostsIndex posts={posts} onSelectPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleHidePost}>
-        <PostsShow post={currentPost} />
+        <PostsShow post={currentPost} onUpdatePost={handleUpdatePost} />
       </Modal>
       <Comments comments={comments} />
     </div>
--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,32 @@
 export function PostsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdatePost(props.post.id, params);
+  };
+
   return (
     <div>
       <h1>{props.post.title}</h1>
       <img src={props.post.image} width="50"></img>
       <p>{props.post.body}</p>
+
+      <h1>Edit post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title:
+          <input defaultValue={props.post.title} name="title" type="text" />
+        </div>
+        <div>
+          Body:
+          <input name="body" defaultValue={props.post.body} type="text" />
+        </div>
+        <div>
+          Image:
+          <input name="image" defaultValue={props.post.image} type="url" />
+        </div>
+        <button type="submit">Submit Edits</button>
+      </form>
     </div>
   );
 }
