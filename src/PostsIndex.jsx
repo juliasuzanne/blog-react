e685081d5ff9c7a214bbf499cf52f7d728b812f4
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-18 13:27:20 -0500

    Add filter to posts index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   console.log(props.posts);
   return (
     <div id="posts-index">
-      <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} id="post">
-          <h2>{post.title}</h2>
-          <img src={post.image} alt="" />
-          <p> {post.body}</p>
-          <button onClick={() => props.onSelectPost(post)}>More Info</button>
-        </div>
-      ))}
+      Search Filter:
+      <input
+        className="container"
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+      />
+      {props.posts
+        .filter((posts) => posts.body.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((post) => (
+          <div key={post.id} id="post">
+            <h2>{post.title}</h2>
+            <img src={post.image} alt="" />
+            <p> {post.body}</p>
+            <button onClick={() => props.onSelectPost(post)}>More Info</button>
+          </div>
+        ))}
     </div>
   );
 }
